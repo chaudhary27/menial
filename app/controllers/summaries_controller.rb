1e08911596c27df4d6a51d4321c6d16c02c2c3f0
Faisal <chaudhary.fsl@gmail.com>
2015-07-24 00:21:51 +0000

    added new and create controller for summaries with simple gem form

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -3,4 +3,19 @@ class SummariesController < ApplicationController
 	def index
 		@summarie = Summarie.order("RANDOM()").first
 	end
+
+	def new
+		@summarie = Summarie.new
+	end
+
+	def create
+		Summarie.create(summarie_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def summarie_params
+		params.require(:summarie).permit(:work, :help, :name)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Menial::Application.routes.draw do
 
   root 'summaries#index'
+
+  resources :summaries
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
